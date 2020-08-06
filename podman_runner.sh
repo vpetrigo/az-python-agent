45dad0af56c9032935432bed8bfc735c499c08a3
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-08-06 22:10:00 +0300

    misc: Add podman_runner example

---
+++ podman_runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+usage () {
+    echo "runner.sh <container tag>"
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+    exit 1
+fi
+
+# Accept the container tag version as the first argument
+TAG=$1
+
+podman run -it -e AZP_POOL="$AZP_POOL" -e AZP_URL="$AZP_URL" -e AZP_TOKEN="$AZP_TOKEN" -e AZP_AGENT_NAME="$AZP_AGENT_NAME" vpetrigo/az-python-agent:$TAG
