e1a55063c337fb87fadf994670d94f81a9508a44
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-08-09 16:56:34 +0300

    misc: Add build script

---
+++ build/builder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -A image
+
+for arch in amd64 arm64 ; do
+    buildah bud -t vpetrigo/az-python-agent:${VER_TAG}-$arch --override-arch $arch --iidfile iid -f .. .
+    image[$arch]=$(cat iid)
+    rm iid
+done
+
+buildah manifest create vpetrigo/az-python-agent:${VER_TAG}
+
+for arch in ${!image[@]} ; do
+    flags=
+    case $arch in
+    arm64|aarch64)
+        flags="--arch arm64 --variant v8"
+    esac
+    buildah manifest add vpetrigo/az-python-agent:${VER_TAG} $flags ${image[$arch]}
+done
+
+buildah manifest push --format v2s2 --all vpetrigo/az-python-agent:${VER_TAG} docker://vpetrigo/az-python-agent:${VER_TAG}
